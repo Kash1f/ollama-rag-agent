5a526f2fc7e9d2427c24e95ca03cb43cd1fdcc84
Kashif <ahmed.kashif1001@gmail.com>
2026-01-17 00:08:02 +0500

    added retriever to retrieve relevant data from the vector db and pass it to the LLM

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 
 model = OllamaLLM(model="llama3.2")
 
-# Inside the string we will specify what the model will actually do
+# template defining model's role and context for answering questions
 template = """
 
 You are expert in answering questions about a pizza restaurant
@@ -15,17 +16,18 @@ Here is the question to answer: {question}
 
 """
 
-# in the chat prompt template we will pass in a reviews variable and a question variable and then the model can respond to that
-
+# creating prompt template and chain to model for processing
 prompt = ChatPromptTemplate.from_template(template) 
-chain = prompt | model # we are chaining the prompt with the model so that when we invoke the chain, it will first format the prompt (with the reviews and question) and then pass it to the model to get a response
+chain = prompt | model 
 
-
+# interactive loop for user questions
 while True:
     print("\n\n--------------------------------")
-    question = input("Ask your question or press q to quit:")
+    question = input("Ask your question or press q to quit: ")
     print("\n\n")
     if question == "q":
         break
-    result = chain.invoke({"reviews": [], "question": question})
-print(result) # we are invoking the chain with an empty list of reviews and a question about the best pizza place in town. The model will then respond based on the prompt we provided earlier.
+
+    reviews = retriever.invoke(question)  # retrieving top 5 relevant reviews from vector DB based on the question
+    result = chain.invoke({"reviews": reviews, "question": question})  # passing reviews and question to chain
+    print(result) 
