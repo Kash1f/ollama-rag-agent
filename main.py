3ecb6700eff052b1d3e0c201917b2878e35b7c26
Kashif <ahmed.kashif1001@gmail.com>
2026-01-16 17:56:21 +0500

    added chat continuity functionality

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
 
-
-# Llama should be running in the bg on your computer and it's going to expose a server or like an HTTP REST API that will communicate with our program(code). So when you pull these models, they are actually running locally on your own computer, and we can trigger Ollama to utilize these models from code in Python, or we can do it diretly from the cmd.
-
-# We will utlize the Ollama model, we will specify the model that I want to use from Ollama, then we can start utilizing the model and kind of invoking it.
-
 model = OllamaLLM(model="llama3.2")
 
 # Inside the string we will specify what the model will actually do
@@ -25,5 +20,10 @@ Here is the question to answer: {question}
 prompt = ChatPromptTemplate.from_template(template) 
 chain = prompt | model # we are chaining the prompt with the model so that when we invoke the chain, it will first format the prompt (with the reviews and question) and then pass it to the model to get a response
 
-result = chain.invoke({"reviews": [], "question": "What is the best pizza place in this town?"})
+
+while True:
+    question = input("Ask your question or press q to quit:")
+    if question == "q":
+        break
+    result = chain.invoke({"reviews": [], "question": question})
 print(result) # we are invoking the chain with an empty list of reviews and a question about the best pizza place in town. The model will then respond based on the prompt we provided earlier.
